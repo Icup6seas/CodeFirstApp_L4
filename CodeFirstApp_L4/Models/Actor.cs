de7465f1f0ec8a3640d2f3b9edb064e0c07d6e34
Chris Cosnowski <chris.cosnowski@gmail.com>
2020-02-08 17:21:53 -0500

    2nd Commit - EF Installed, 3 data models created

---
+++ CodeFirstApp_L4/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirstApp_L4.Models
+{
+    public class Actor
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime YearActive { get; set; }
+        public DateTime YearRetired { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ CodeFirstApp_L4/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeFirstApp_L4.Models
+{
+    public enum Rating
+    {
+        E, PG, M, R
+    }
+
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public int GenreID { get; set; }
+        public int ActorID { get; set; }
+        public Rating? Rating { get; set; }
+
+        public virtual Genre Genre { get; set; }
+        public virtual Actor Actor { get; set; }
+    }
+}
