f0727520d32b8f65e89c9d984403c43c1d08afa0
Chris Cosnowski <chris.cosnowski@gmail.com>
2020-03-15 00:29:54 -0400

    L9 - Added most of the skeleton for Authentication: -IdentiyConfig.cs, Startup.Auth.cs, Membership.cs, ManageController.cs, MemberDZBContext.cs, MemberInitializer.cs, IdentityModels.cs

---
+++ CodeFirstApp_L4/Controllers/ManageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstApp_L4.Controllers
+{
+    public class ManageController
+    {
+    }
+}
